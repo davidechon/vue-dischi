f14498da588e1e4056d7578755feb4059f1a5949
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-05-06 16:14:23 +0200

    AppGrid wip

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -10,12 +10,41 @@
       "year": "1988"
     }
 } -->
-  <app-grid />
+  <div class="container">
+    <!-- 
+    img 
+    titolo
+    autore
+    anno
+    -->
+    <img :src="(album, index )in albumList" :key="album.index" :alt="album.title">
+    <div :class="title">{{album.title}}</div>
+    <div :class="author">{{album.author}}</div>
+    <div :class="year">{{album.year}}</div>
+
+  </div>
 </template>
 
 <script>
 export default {
-name: 'AppGrid',
+  name: 'AppGrid',
+  components:{
+
+  }
+  data(){
+    return{
+      albumList: [],
+      apiPath: 'https://flynn.boolean.careers/exercises/api/array/music',
+    },
+    mounted(){
+      axios.loading(this.apiPath + 'albums').then((res)=>{
+        console.log(res);
+        this.albumList = [...res.data];
+      }).catch((error)=>{
+        console.log(error);
+      })
+    }
+  }
 }
 </script>
 
